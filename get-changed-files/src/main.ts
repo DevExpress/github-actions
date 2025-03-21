d6c2a6fa774c00d1a440891db9b2ff0863bdd77a
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2025-03-21 18:47:51 +0200

    Remove file output

--- get-changed-files/src/main.ts
+++ get-changed-files/src/main.ts
@@ -14,7 +14,6 @@ async function run(): Promise<void> {
     try {
         const pathPatterns = core.getInput(inputs.PATHS).split(';');
         const token = core.getInput(inputs.GH_TOKEN, { required: true });
-        const output = core.getInput(inputs.OUTPUT, { required: true });
 
         console.log('patterns: ' + JSON.stringify(pathPatterns, undefined, 2));
 
@@ -22,9 +21,6 @@ async function run(): Promise<void> {
         const filteredFiles = pathPatterns.length > 0
             ? changedFiles.filter(({ path }) => testPath(path, pathPatterns))
             : changedFiles;
-
-        ensureDir(output);
-        fs.writeFileSync(output, JSON.stringify(filteredFiles.map(({ path }) => ({ filename: path })), undefined, 2));
 
         setOutputs({
             json: JSON.stringify({
