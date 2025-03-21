d6b1cb8bd55f2c4724c8339c8fb7c9df165a1131
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2025-03-21 18:56:44 +0200

    Fix paths filtering in the case of empty filter

--- common/src/path-utils.ts
+++ common/src/path-utils.ts
@@ -24,11 +24,13 @@ function filterPathsImpl(
     paths: string[],
     patterns: string[],
 ): string[] {
-    return paths.filter(path => {
-        return patterns.reduce((prevResult, pattern) => {
-            return pattern.startsWith(NEGATION)
-                ? prevResult && !match(path, pattern.substring(1))
-                : prevResult || match(path, pattern);
-        }, false);
-    });
+    return patterns?.length === 0
+        ? paths
+        : paths.filter(path => {
+            return patterns.reduce((prevResult, pattern) => {
+                return pattern.startsWith(NEGATION)
+                    ? prevResult && !match(path, pattern.substring(1))
+                    : prevResult || match(path, pattern);
+            }, false);
+        });
 }
